573f9e9d183eabe816f329764c2a5399b193da64
Charles Moulliard <ch007m@gmail.com>
2014-12-17 13:49:15 +0100

    Add module quickstart-persistence

---
+++ quickstarts/persistence/src/main/java/org/apache/camel/cdi/example3/BootStrap.java
@@ -0,0 +1,52 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.cdi.example3;
+
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.cdi.ContextNameLiteral;
+import org.jboss.weld.environment.se.StartMain;
+import org.jboss.weld.environment.se.WeldContainer;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Destroyed;
+import javax.enterprise.context.Initialized;
+import javax.enterprise.event.Observes;
+import java.util.concurrent.CountDownLatch;
+
+public class BootStrap {
+
+    void start(@Observes @Initialized(ApplicationScoped.class) Object event) {
+        System.out.println("Camel CDI :: Example 3 will be started");
+        // The context is started in the @PostConstruct lifecycle callback (see class SimpleCamelContext)
+    }
+
+    void shutdown(@Observes @Destroyed(ApplicationScoped.class) Object event) {
+        System.out.println("Camel CDI :: Example 3 will be stopped");
+        // The context is stopped in the @PreDestroy lifecycle callback (see class SimpleCamelContext)
+    }
+
+    public static void main(String[] args) throws Exception {
+        WeldContainer container = new StartMain(args).go();
+        // Get a reference to the Camel context named "simple"
+        CamelContext context = container.instance().select(CamelContext.class, new ContextNameLiteral("simple")).get();
+        // Start it
+        context.start();
+        // And wait until the JVM exits
+        new CountDownLatch(1).await();
+    }
+}
---
+++ quickstarts/persistence/src/main/java/org/apache/camel/cdi/example3/db/DataSourceFactory.java
@@ -0,0 +1,24 @@
+package org.apache.camel.cdi.example3.db;
+
+import org.h2.jdbcx.JdbcDataSource;
+
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.inject.Named;
+import javax.sql.DataSource;
+
+public class DataSourceFactory {
+
+    @Produces @DataSourceDefinition @Named("myDataSource")
+    public DataSource initDataSource(InjectionPoint injectionPoint) {
+
+        DataSourceDefinition dsdef = injectionPoint.getAnnotated().getAnnotation(DataSourceDefinition.class);
+
+        JdbcDataSource ds = new JdbcDataSource();
+        ds.setUrl(dsdef.url());
+        ds.setUser(dsdef.user());
+        ds.setPassword(dsdef.password());
+
+        return (DataSource)ds;
+    }
+}
